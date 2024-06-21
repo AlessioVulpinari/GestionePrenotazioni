49c639310669832bd02189ecb4c361b8a6a81c9c
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-06-21 10:41:56 +0200

    Initial commit

---
+++ src/test/java/alessiovulpinari/u2_w1_d5_Java/U2W1D5JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package alessiovulpinari.u2_w1_d5_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W1D5JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
